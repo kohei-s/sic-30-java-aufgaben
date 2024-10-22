25e50279880d687b7e2e19f2639578ed99c79079
kohei-s <kicoyuz@gmail.com>
2024-10-22 10:19:14 +0200

    add 1-3 Das Einmaleins Aufgabe

---
+++ One/src/DasEinmaleins.java
@@ -0,0 +1,9 @@
+public class DasEinmaleins {
+    public void tables(int number) {
+        System.out.println("The tables up to ten of the number: " + number);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number * i);
+        }
+
+    }
+}
--- One/src/Main.java
+++ One/src/Main.java
@@ -5,9 +5,14 @@ public class Main {
         z.checkNumber(-100);
         z.checkNumber(0);*/
 
-        GeradeOderUngeradeZahl g = new GeradeOderUngeradeZahl();
+        /*GeradeOderUngeradeZahl g = new GeradeOderUngeradeZahl();
         g.checkOddOrEven(100);
         g.checkOddOrEven(101);
-        g.checkOddOrEven(0);
+        g.checkOddOrEven(0);*/
+
+        DasEinmaleins d = new DasEinmaleins();
+        d.tables(1);
+        d.tables(20);
+        d.tables(123);
     }
 }
