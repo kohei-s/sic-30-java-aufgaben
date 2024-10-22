e10eb312c7815064da2ec605d900e9bebe15461e
kohei-s <kicoyuz@gmail.com>
2024-10-22 09:56:46 +0200

    add Main class to the first module and gitignore

---
+++ One/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        ZahlenVergleich z = new ZahlenVergleich();
+        z.checkNumber(100);
+        z.checkNumber(-100);
+        z.checkNumber(0);
+    }
+}
