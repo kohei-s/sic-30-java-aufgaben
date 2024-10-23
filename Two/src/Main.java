a2e451306b24ea0458be84ed805b98de97922bbd
kohei-s <kicoyuz@gmail.com>
2024-10-23 21:58:38 +0200

    add 2-2 Zeichen umwandeln Aufgabe

--- Two/src/Main.java
+++ Two/src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        WortlaengeZaehlen w = new WortlaengeZaehlen();
+        /*WortlaengeZaehlen w = new WortlaengeZaehlen();
         w.coountCharacter("abcdef");
         w.coountCharacter("Klfhn390");
-        w.coountCharacter("lP0123eb)(");
+        w.coountCharacter("lP0123eb)(");*/
+
+        ZeichenUmwandeln z = new ZeichenUmwandeln();
+        z.changeCase("UkGhsbdUI");
+        z.changeCase("kgjshTrfsHHHJ");
+        z.changeCase("42ezhJ78KO");
     }
 }
